6bf5ce058a93b042777bb941c0839cb5c9e64f3e
Vincent Sellier <vsellier@exoplatform.com>
2020-04-02 19:09:35 +0200

    ITOP-4847 fix EHLO rejection due to simple hostname

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -eu
 
 DEBUG=${DEBUG-false}
-HOSTNAME=${HOSTNAME-smtp}
+# use the full qualified hostname to pass the fqdn check on the EHLO command
+FQDN=$(hostname -f)
 RELAY_DOMAINS= #Empty to relay all emails
 
-postconf -e myhostname=${HOSTNAME}
+postconf -e myhostname="${FQDN}"
 postconf -e relay_domains=${RELAY_DOMAINS}
 postconf -e smtpd_sasl_auth_enable=no
 postconf -e "mynetworks= 127.0.0.0/8 172.16.0.0/12"
