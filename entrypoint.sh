2a8898b4d1738e9bdcadcce5fb9a998e81fdc6fb
bourasom <obouras@exoplatform.com>
2021-02-22 10:21:19 +0100

    ITOP-22116: add docker allowed network

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ RELAY_DOMAINS= #Empty to relay all emails
 postconf -e myhostname="${FQDN}"
 postconf -e relay_domains=${RELAY_DOMAINS}
 postconf -e smtpd_sasl_auth_enable=no
-postconf -e "mynetworks= 127.0.0.0/8 172.16.0.0/12"
+postconf -e "mynetworks= 127.0.0.0/8 172.16.0.0/12 192.0.0.0/8"
 postconf -# mydestination
 postconf -F '*/*/chroot = n'
 
